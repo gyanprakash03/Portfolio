dd4a99a866bc2243b03948d2c43de18b7ff6fd94
gyanprakash03 <gyan091203@gmail.com>
2025-07-12 14:20:41 +0530

    debugging

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
       {/* <Experience /> */}
       <Skills />
       <Projects />
-      <Education />
+      {/* <Education /> */}
       {/* <Blog blogs={blogs} /> */}
       <ContactSection />
     </div>
