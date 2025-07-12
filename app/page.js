dcc8b1bc1cb3cd9f2ee2c3f0c2a2e16cd53627ee
gyanprakash03 <gyan091203@gmail.com>
2025-07-12 14:25:38 +0530

    fixing page

--- app/page.js
+++ app/page.js
@@ -2,11 +2,17 @@ import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
+// import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+
+import dynamic from 'next/dynamic';
+
+const Education = dynamic(() => import('./components/homepage/education'), {
+  ssr: false,
+});
 
 export default function Home() {
 
